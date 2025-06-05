00a2c4d3b63df1d07bf735279dde707e15768887
Dave02233 <davidecannistraro02@gmail.com>
2025-06-05 22:28:36 +0200

    Fix Img Loading

--- Risorse/script.js
+++ Risorse/script.js
@@ -7,11 +7,13 @@ function manageTrack() {
     if (!track) return;
 
     // Movimento lineare infinito
-    let speed = 1.3; // pixel per step
+    let speed = 0.0; // pixel per step
 
     function move() {
 
         gallery.scrollLeft += speed;
+        speed < 1.3 ? speed += 0.005 : speed = 1.3;
+        //console.log(speed);
         const firstImg = track.firstElementChild;
 
         if (firstImg) {
@@ -28,5 +30,6 @@ function manageTrack() {
     move();
 }
 
-manageTrack();
+// Avvia l'animazione dopo 4 secondi dal caricamento
+setTimeout(manageTrack, 4000);
 
